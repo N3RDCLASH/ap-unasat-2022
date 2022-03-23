1e79a26d952e32d374026d6ab1ad08aa1460e9c8
Joel Naarendorp <joelhammen11@gmail.com>
2022-03-23 15:40:56 -0300

    added comments

--- GESS.ino
+++ GESS.ino
@@ -30,208 +30,215 @@ LiquidCrystal_I2C lcd(0x27, 20, 4);
 
 void setup()
 {
-    Serial.begin(9600);
-    lcd.init();
-    lcd.backlight();
-    pinMode(BLUE_LED, OUTPUT);
-    pinMode(RED_LED, OUTPUT);
-    pinMode(YELLOW_LED, OUTPUT);
-    pinMode(SOLAR_INPUT, INPUT);
-    pinMode(BATTERY_RELAY, OUTPUT);
-    pinMode(SOLAR_OUTPUT, OUTPUT);
-    pinMode(BATTERY_OUTPUT, OUTPUT);
+  Serial.begin(9600);
+  lcd.init();
+  lcd.backlight();
+  pinMode(BLUE_LED, OUTPUT);
+  pinMode(RED_LED, OUTPUT);
+  pinMode(YELLOW_LED, OUTPUT);
+  pinMode(SOLAR_INPUT, INPUT);
+  pinMode(BATTERY_RELAY, OUTPUT);
+  pinMode(SOLAR_OUTPUT, OUTPUT);
+  pinMode(BATTERY_OUTPUT, OUTPUT);
 
-    digitalWrite(RED_LED, HIGH);
+  digitalWrite(RED_LED, HIGH);
 
-    digitalWrite(BUZZER, HIGH);
-    delay(500);
-    digitalWrite(BUZZER, LOW);
+  digitalWrite(BUZZER, HIGH);
+  delay(500);
+  digitalWrite(BUZZER, LOW);
 
-    digitalWrite(BATTERY_RELAY, HIGH);
+  digitalWrite(BATTERY_RELAY, HIGH);
 
-    setup_completed = true;
+  setup_completed = true;
 }
 
 // todo: add comments
 void loop()
 {
-    // Solar Panel
-    int solar_reading = analogRead(SOLAR_INPUT);
-    float solar_voltage = calculateVoltage(solar_reading);
+  // Solar Panel
+  int solar_reading = analogRead(SOLAR_INPUT);
+  float solar_voltage = calculateVoltage(solar_reading);
 
-    // Battery Values
-    int battery_reading = analogRead(BATTERY_INPUT);
-    float battery_voltage = calculateVoltage(battery_reading);
-    float battery_percentage = calculateBatteryPercentage(battery_voltage);
+  // Battery Values
+  int battery_reading = analogRead(BATTERY_INPUT);
+  float battery_voltage = calculateVoltage(battery_reading);
+  float battery_percentage = calculateBatteryPercentage(battery_voltage);
 
-    solar_connected = solarArrayConnected(solar_voltage);
-    battery_connected = batteryConnected(battery_voltage);
+  // check connections
+  solar_connected = solarArrayConnected(solar_voltage);
+  battery_connected = batteryConnected(battery_voltage);
 
-    while (!solar_connected || !battery_connected)
-    {
-        digitalWrite(RED_LED, HIGH);
-        delay(200);
-        digitalWrite(RED_LED, LOW);
-        delay(200);
-        return;
-    }
+  // flash red led while one of the power inputs in not connected
+  while (!solar_connected || !battery_connected)
+  {
+    // flash red led
+    digitalWrite(RED_LED, HIGH);
+    delay(200);
+    digitalWrite(RED_LED, LOW);
+    delay(200);
+    return;
+  }
 
-    // Print values to console
-    printPowerLevelsToConsole(solar_voltage, battery_voltage, battery_percentage);
+  // Print values to console
+  printPowerLevelsToConsole(solar_voltage, battery_voltage, battery_percentage);
 
-    // Print values to lcd
-    printPowerLevelsToLcd(solar_voltage, battery_voltage, battery_percentage);
+  // Print values to lcd
+  printPowerLevelsToLcd(solar_voltage, battery_voltage, battery_percentage);
 
-    // Monitor power
-    monitorPowerLevels(solar_voltage, battery_voltage);
+  // Monitor power levels
+  monitorPowerLevels(solar_voltage, battery_voltage);
 
-    Serial.println("---------");
+  Serial.println("--------------");
 }
 
 void monitorPowerLevels(float solar_voltage, float battery_voltage)
 {
-    // mode: Battery Charging
-    if (solar_voltage >= SOLAR_MIN_CHARGE_VOLTAGE)
+  if (solar_voltage >= SOLAR_MIN_CHARGE_VOLTAGE)
+  {
+    // mode: battery charging
+    turnOnSolarIndicatorLED();
+    if (BATTERY_MIN_VOLTAGE < battery_voltage < BATTERY_MAX_VOLTAGE)
     {
-        turnOnSolarIndicatorLED();
-        if (BATTERY_MIN_VOLTAGE < battery_voltage < BATTERY_MAX_VOLTAGE)
-        {
-            // connect the solar voltage to charge the battery
-            digitalWrite(BATTERY_RELAY, HIGH);
-            // set solar panel as output
-            digitalWrite(SOLAR_OUTPUT, HIGH);
-            // remove battery as output
-            digitalWrite(BATTERY_OUTPUT, LOW);
-            return;
-        }
-
-        if (battery_voltage == BATTERY_MAX_VOLTAGE)
-        {
-            // disconnect the solar voltage
-            digitalWrite(BATTERY_RELAY, LOW);
-            // set solar panel as output
-            digitalWrite(SOLAR_OUTPUT, HIGH);
-            // remove battery as output
-            digitalWrite(BATTERY_OUTPUT, LOW);
-            return;
-        }
+      // connect the solar voltage to charge the battery
+      digitalWrite(BATTERY_RELAY, HIGH);
+      // set solar panel as output
+      digitalWrite(SOLAR_OUTPUT, HIGH);
+      // remove battery as output
+      digitalWrite(BATTERY_OUTPUT, LOW);
+      return;
     }
 
-    // mode: Battery Charging
-    if (solar_voltage < SOLAR_MIN_CHARGE_VOLTAGE)
+    // mode: battery stops charging
+    if (battery_voltage == BATTERY_MAX_VOLTAGE)
     {
-        turnOnBatteryIndicatorLED();
-        // disconnect the solar voltage to charge the battery
-        digitalWrite(BATTERY_RELAY, LOW);
-
-        if (battery_voltage == BATTERY_MAX_VOLTAGE)
-        {
-            // set solar panel as output
-            digitalWrite(SOLAR_OUTPUT, HIGH);
-            // remove battery as output
-            digitalWrite(BATTERY_OUTPUT, LOW);
-            return;
-        }
+      // disconnect the solar voltage
+      digitalWrite(BATTERY_RELAY, LOW);
+      // set solar panel as output
+      digitalWrite(SOLAR_OUTPUT, HIGH);
+      // remove battery as output
+      digitalWrite(BATTERY_OUTPUT, LOW);
+      return;
     }
+  }
+
+  // mode: solar panel voltage low
+  if (solar_voltage < SOLAR_MIN_CHARGE_VOLTAGE)
+  {
+    turnOnBatteryIndicatorLED();
+    // disconnect the solar voltage to charge the battery
+    digitalWrite(BATTERY_RELAY, LOW);
+
+    if (battery_voltage == BATTERY_MAX_VOLTAGE)
+    {
+      // set solar panel as output
+      digitalWrite(SOLAR_OUTPUT, HIGH);
+      // remove battery as output
+      digitalWrite(BATTERY_OUTPUT, LOW);
+      return;
+    }
+  }
 }
 
 void printPowerLevelsToConsole(float solar_voltage, float battery_voltage, float battery_percentage)
 {
-    // Print values to console (Serial Monitor)
-    Serial.print("Solar Voltage: ");
-    Serial.println(solar_voltage);
-    Serial.print("Battery Voltage: ");
-    Serial.println(battery_voltage);
-    Serial.print("Battery Percentage: ");
-    Serial.println(battery_percentage);
+  // Print values to console (Serial Monitor)
+  Serial.print("Solar Voltage: ");
+  Serial.println(solar_voltage);
+  Serial.print("Battery Voltage: ");
+  Serial.println(battery_voltage);
+  Serial.print("Battery Percentage: ");
+  Serial.println(battery_percentage);
 }
-
+// Print values to LCD
 void printPowerLevelsToLcd(float solar_voltage, float battery_voltage, float battery_percentage)
 {
-    lcd.setCursor(0, 0);
-    lcd.print("Solar:");
-    lcd.print(solar_voltage);
+  lcd.setCursor(0, 0);
+  lcd.print("Solar:");
+  lcd.print(solar_voltage);
 
-    lcd.setCursor(0, 1);
-    lcd.print("Battery:");
-    lcd.print(battery_voltage);
+  lcd.setCursor(0, 1);
+  lcd.print("Battery:");
+  lcd.print(battery_voltage);
 }
 
+// set blue led as solar mode indicator
 void turnOnSolarIndicatorLED()
 {
-    digitalWrite(YELLOW_LED, LOW);
-    digitalWrite(BLUE_LED, HIGH);
+  digitalWrite(YELLOW_LED, LOW);
+  digitalWrite(BLUE_LED, HIGH);
 }
-
+// set yellow led as battery mode indicator
 void turnOnBatteryIndicatorLED()
 {
-    digitalWrite(YELLOW_LED, HIGH);
-    digitalWrite(BLUE_LED, LOW);
+  digitalWrite(YELLOW_LED, HIGH);
+  digitalWrite(BLUE_LED, LOW);
 }
 
-// start calculation methods
+// --- start calculation methods ---
 float calculateVoltage(int reading)
 {
-    return ((reading * 5.0) / DIGITAL_CONSTANT) * 5.5;
+  return ((reading * 5.0) / DIGITAL_CONSTANT) * 5.5;
 }
 
 float calculateCurrent(float voltage)
 {
-    return voltage / DIGITAL_CONSTANT;
+  return voltage / DIGITAL_CONSTANT;
 }
 
+// calculate the battery percentage using the current battery voltage
 float calculateBatteryPercentage(float current_voltage)
 {
-    return ((current_voltage - BATTERY_MIN_VOLTAGE) / (BATTERY_MAX_VOLTAGE - BATTERY_MIN_VOLTAGE)) * 100;
+  return ((current_voltage - BATTERY_MIN_VOLTAGE) / (BATTERY_MAX_VOLTAGE - BATTERY_MIN_VOLTAGE)) * 100;
 }
-// end calculation methods
+// --- end calculation methods ---
 
-// vanaf hier begint het checken van connecties aan de GESS
+// --- start connection check methods ---
 bool solarArrayConnected(float voltage)
 {
-    if (voltage != 0)
+  if (voltage != 0)
+  {
+    if (!setup_completed)
     {
-        if (!setup_completed)
-        {
-            lcd.setCursor(0, 1);
-            lcd.print("Solar Array Connected");
-            Serial.println("Solar Array Connected");
-        }
-        return true;
+      lcd.setCursor(0, 1);
+      lcd.print("Solar Array Connected");
+      Serial.println("Solar Array Connected");
     }
-    lcd.setCursor(0, 1);
-    lcd.print("Solar Array Not Connected");
-    Serial.println("Solar Array Not Connected");
-    return false;
+    return true;
+  }
+  lcd.setCursor(0, 1);
+  lcd.print("Solar Array Not Connected");
+  Serial.println("Solar Array Not Connected");
+  return false;
 }
 
 bool batteryConnected(float voltage)
 {
-    if (voltage >= BATTERY_MIN_VOLTAGE)
-    { // insert logic here - coming soon
-        if (!setup_completed)
-        {
-            lcd.setCursor(0, 1);
-            lcd.print("Battery Connected");
-            Serial.println("Battery Connected");
-        }
-        return true;
+  if (voltage >= BATTERY_MIN_VOLTAGE)
+  { // insert logic here - coming soon
+    if (!setup_completed)
+    {
+      lcd.setCursor(0, 1);
+      lcd.print("Battery Connected");
+      Serial.println("Battery Connected");
     }
-    lcd.setCursor(0, 1);
-    lcd.print("Battery Not Connected");
-    Serial.println("Battery Not Connected");
-    return false;
+    return true;
+  }
+  lcd.setCursor(0, 1);
+  lcd.print("Battery Not Connected");
+  Serial.println("Battery Not Connected");
+  return false;
 }
+// --- start connection check methods ---
 
 bool outputWires()
 { // L+- komt hier
-    if (true)
-    { // insert logic here -coming soon
-        lcd.setCursor(0, 1);
-        lcd.print("Output wires connected");
-        return true;
-    }
+  if (true)
+  { // insert logic here -coming soon
     lcd.setCursor(0, 1);
-    lcd.print("Output Wires Not Connected");
-    return false;
+    lcd.print("Output wires connected");
+    return true;
+  }
+  lcd.setCursor(0, 1);
+  lcd.print("Output Wires Not Connected");
+  return false;
 }
